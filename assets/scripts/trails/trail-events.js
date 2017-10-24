d4cad533636d41125f1b7a70528359177e81fb3d
mmorgan412 <melanie.morgan@libertymutual.com>
2017-10-24 15:00:42 -0400

    Remove console.logs

--- assets/scripts/trails/trail-events.js
+++ assets/scripts/trails/trail-events.js
@@ -81,8 +81,6 @@ const updateTrail = () => {
 const searchTrails = (event) => {
   event.preventDefault()
   const data = getFormFields(event.target)
-  console.log('data.rating is ', data.trail.rating)
-  console.log('data is ', data)
   trailsApi.searchTrails(data.trail.rating)
     .then(trailsUi.searchTrailsSuccess)
     .then(deleteTrail)
